62f7ffda6874b1755d0b0d6b58dfdd37d758bbb3
XunStarRock <1308435433@qq.com>
2023-10-11 22:54:30 +0800

    创建静态图库选项卡
    支持了基于UI的基础图片库生成

--- main.gd
+++ main.gd
@@ -10,10 +10,17 @@
 
 extends Control
 
+# 文档生成 - 表单元素
 @onready var doc_name_txt = %docNameTxt
 @onready var sub_title_txt = %subTitleTxt
 @onready var root_dir_txt = %rootDirTxt
 @onready var file_dialog = %FileDialog
+# 图片库生成 - 表单元素
+@onready var album_name_txt = %albumNameTxt
+@onready var alnum_root_dir_txt = %alnumRootDirTxt
+@onready var img_dir_name_txt = %imgDirNameTxt
+@onready var links_txt = %linksTxt
+
 
 # ================================= 信号处理 ================================= 
 
@@ -33,6 +40,23 @@ func _on_create_doc_btn_pressed():
 	var doc = myDocStation.new(doc_name_txt.text,sub_title_txt.text,root_dir_txt.text)
 	doc.create_station()
 
+# 生成图片库
+func _on_create_album_btn_pressed():
+	# 常用网址
+	var links = get_links()
+	
+	var station = myPicStation.new(album_name_txt.text,alnum_root_dir_txt.text,img_dir_name_txt.text,links)
+	station.create_station()
+
+# 设定图库根目录
+func _on_album_set_dir_btn_pressed():
+	# 打开文件对话框
+	file_dialog.popup_centered_ratio()
+	# 选中文件夹后
+	file_dialog.connect("dir_selected",func(dir):
+		alnum_root_dir_txt.text = dir
+	)
+
 # ================================= 自定义函数 =================================
 
 # 返回指定路径的文件夹名
@@ -40,3 +64,17 @@ func get_dir_name(dir_path:String) -> String:
 	var count = dir_path.get_slice_count("/")
 	var idx = count - 2 if dir_path.ends_with("/") else count - 1
 	return dir_path.get_slice("/",idx)
+
+# 根据输入的链接字符串生成并返回可以使用的二维数组结构
+func get_links() -> Array: 
+	var arr:Array = []
+	if links_txt.text == "":
+		return arr
+	var links_str:String = links_txt.text
+	for link in links_str.split("\n"):
+		arr.append(link.split(","))
+	return arr
+
+
+
+
