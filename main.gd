99ee6b4da5fe8113b5f94265648b58a92d6dfa90
XunStarRock <1308435433@qq.com>
2023-10-12 21:57:04 +0800

    实现Godot整项目gd转MD文档
    - 添加gd2MD类
    - 创建gd2MD选项卡和UI界面

--- main.gd
+++ main.gd
@@ -5,21 +5,24 @@
 # 作者：巽星石
 # Godot版本：v4.1.2.stable.official [399c9dc39]
 # 创建时间：2023年10月11日19:26:45
-# 最后修改时间：2023年10月11日21:26:51
+# 最后修改时间：2023年10月12日21:55:28
 # ========================================================
 
 extends Control
 
-# 文档生成 - 表单元素
+# md文档  ==> html文档 - 表单元素
 @onready var doc_name_txt = %docNameTxt
 @onready var sub_title_txt = %subTitleTxt
 @onready var root_dir_txt = %rootDirTxt
 @onready var file_dialog = %FileDialog
-# 图片库生成 - 表单元素
+# 本地图片文件夹 ==> 图片库静态站 - 表单元素
 @onready var album_name_txt = %albumNameTxt
 @onready var alnum_root_dir_txt = %alnumRootDirTxt
 @onready var img_dir_name_txt = %imgDirNameTxt
 @onready var links_txt = %linksTxt
+# Godot项目所有.gd文件 ==> .md文档 - 表单元素
+@onready var gd_root_dir_txt = %gdRootDirTxt
+@onready var gd_2_md_root_dir_txt = %gd2mdRootDirTxt
 
 
 # ================================= 信号处理 ================================= 
@@ -57,6 +60,30 @@ func _on_album_set_dir_btn_pressed():
 		alnum_root_dir_txt.text = dir
 	)
 
+# 选择Godot项目根目录
+func _on_gd_set_dir_btn_pressed():
+	# 打开文件对话框
+	file_dialog.popup_centered_ratio()
+	# 选中文件夹后
+	file_dialog.connect("dir_selected",func(dir):
+		gd_root_dir_txt.text = dir
+	)
+
+# 选择Godot项目导出MD文档根目录
+func _on_gd_2_md_set_dir_btn_pressed():
+	# 打开文件对话框
+	file_dialog.popup_centered_ratio()
+	# 选中文件夹后
+	file_dialog.connect("dir_selected",func(dir):
+		gd_2_md_root_dir_txt.text = dir
+	)
+
+# 基于Godot项目生成.md文档
+func _on_create_md_doc_btn_pressed():
+	var md = gd2MD.new(gd_root_dir_txt.text,gd_2_md_root_dir_txt.text)
+	md.do_it()
+
+
 # ================================= 自定义函数 =================================
 
 # 返回指定路径的文件夹名
@@ -74,7 +101,5 @@ func get_links() -> Array:
 	for link in links_str.split("\n"):
 		arr.append(link.split(","))
 	return arr
-
-
 
 
